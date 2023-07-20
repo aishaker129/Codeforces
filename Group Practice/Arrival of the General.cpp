511e4f1f3749bc68144e1a47e0c67937c793139c
Md Akhlakul Islam <109545500+aishaker129@users.noreply.github.com>
2023-07-20 11:52:22 +0600

    Add files via upload

---
+++ Group Practice/Arrival of the General.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+ int n;
+ cin>>n;
+ int arr[n];
+ for(int i=0; i<n; i++){
+    cin>>arr[i];
+ }
+ int ma = 0, mi = 101, maxi, mini;
+
+ for(int i=0; i<n; i++){
+    if(arr[i]>ma){
+        ma = arr[i];
+        maxi = i;
+    }
+    if(arr[i]<=mi){
+        mi = arr[i];
+        mini = i;
+    }
+ }
+ if(maxi>mini){
+    mini++;
+ }
+
+ cout<<(maxi+(n-1)-mini)<<endl;
+
+ return 0;
+}
